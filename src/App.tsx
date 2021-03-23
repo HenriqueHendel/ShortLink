2202423255b4a5175487b5b7520535026fb1ac1f
Henrique Lopes <henriquehendell@gmail.com>
2021-03-22 22:53:17 -0300

    feat: initial ts config

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+ import React from 'react';
+ import {
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ const App = () => {
+ 
+   return (
+    <View />
+   );
+ };
+
+ 
+ export default App;
+ 
